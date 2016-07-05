1c20ffded1146ab9d8982239b5a0a9c2c6d83b2f
jmrunkle <jmrunkle@gmail.com>
2016-07-05 08:03:40 -0700

    Use grunt to make new images

--- BlogProject/Project-Part-1-Start/Gruntfile.js
+++ BlogProject/Project-Part-1-Start/Gruntfile.js
@@ -14,13 +14,9 @@ module.exports = function(grunt) {
         options: {
           engine: 'im',
           sizes: [{
-            /*
-            Change these:
-            
-            width: ,
-            suffix: ,
-            quality:
-            */
+            width: '800px',
+            suffix: 'png',
+            quality: '75'
           }]
         },
 
@@ -64,7 +60,7 @@ module.exports = function(grunt) {
       },
     },
   });
-  
+
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
